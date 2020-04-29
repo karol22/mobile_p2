2889fcaf08fdae70d42b8b49517e2c3cdcc22dbd
Jorge Pietra Santa <a00226522@itesm.mx>
2020-04-29 06:57:11 -0700

    Added things from Brian's branch

---
+++ app/src/main/java/com/example/android/movilesp2/Character.java
@@ -0,0 +1,27 @@
+package com.example.android.movilesp2;
+
+public class Character {
+
+    private String name,age;
+
+    public Character(String name, String age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
